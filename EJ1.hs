78425642d1a75a83b9ec46ad3b6e5ae520b7dc63
MatiMCode <matiasmcxd12@gmail.com>
2024-04-08 13:01:58 -0300

    Cambie el nombre del archivo

--- EJ1.hs
+++ EJ1.hs
@@ -1,5 +1,5 @@
 --ghci despues :l test.hs para CARGAR el archivo nomas, despues escribir doubleMe
---quit para salir
+-- :quit para salir
 --  :r es para recargar el coso, lo pones en ghci no en main
 
 doubleMe :: Int -> Int
@@ -9,3 +9,24 @@ signo :: Int -> Int
 signo x | x > 0 = 1   --este signito significa que estas haciendo cosas logicas, tipo preposiciones
         | x < 0 = -1
         | otherwise = 0 --aaa
+
+
+--ejercicio 1:
+
+f :: Int -> Int
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+g :: Int -> Int
+g n |n==8=16
+    |n==16=4
+    |n==131=1
+
+--h = fog
+
+h :: Int -> Int
+h x = f(g x)
+
+k :: Int -> Int
+k x = g(f x)
