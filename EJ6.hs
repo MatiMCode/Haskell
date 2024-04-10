bb1c7ccb51df5a4ddd950778280575c9d2dfd70b
Matas <matiasmcxd12@gmail>
2024-04-10 13:20:31 -0300

    termine el 6

--- EJ6.hs
+++ EJ6.hs
@@ -1,2 +1,3 @@
 bisiesto :: Int -> Bool
-bisiesto anio = (mod anio 4 /=0) || ((mod anio 100 == 0)||(mod anio 400 /= 0))
+bisiesto anio | (mod anio 4 /=0) || ((mod anio 100 == 0)&&(mod anio 400 /= 0)) = False
+              | otherwise = True
