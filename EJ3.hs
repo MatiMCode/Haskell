7234af008335bc99c170093af7a467928f5e9b59
Matas <matiasmcxd12@gmail>
2024-04-10 11:50:02 -0300

    Ej3 y nuevo ejercicio

--- EJ3.hs
+++ EJ3.hs
@@ -1,3 +1,8 @@
+--ahh basicamente, a al cuadrado eso deberia ser cancelado por a*b*c 
+
+
+--a*a + a*b*x =0
+--x = -(a)/b
+
 estanRelacionados :: Int -> Int -> Bool
-estanRelacionados a b | a*a + a*b == 0 = True
-                      | otherwise = False
+estanRelacionados a b = (a*a + a*b*(div (-a) b) ==0) && (div (-a) b/=0)  
