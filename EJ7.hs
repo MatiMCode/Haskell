81dc84f985ecc1a49b1c8224a572e480f276c215
Matas <matiasmcxd12@gmail>
2024-04-10 13:56:16 -0300

    termine el 7

---
+++ EJ7.hs
@@ -0,0 +1,6 @@
+absoluto :: Float -> Float
+absoluto x | x>=0 = x
+           | otherwise = -x
+
+distanciaManhattan :: (Float,Float,Float) -> (Float,Float,Float) -> Float
+distanciaManhattan (a,b,c) (d,e,f) = absoluto (a-d) + absoluto(b-e) + absoluto(c-f)
