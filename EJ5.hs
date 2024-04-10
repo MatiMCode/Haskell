ae8d4a46412545d0df0820c3fdd8e68e707e7824
Matas <matiasmcxd12@gmail>
2024-04-10 13:15:46 -0300

    termine el 5, empece el 6

---
+++ EJ5.hs
@@ -0,0 +1,13 @@
+--todosMenores, recibe ZxZxZ que devuelve bool, 
+
+
+f :: Int -> Int
+f n | n<=7 = n^2
+    | n>7 = 2*n-1
+
+g:: Int -> Int
+g n | mod n 2 == 0 = div n 2
+    | otherwise = 3*n+1
+
+todosMenores :: (Int,Int,Int) -> Bool
+todosMenores (a,b,c) = (f a> g a) && (f b > g b) && (f c > g c)
---
+++ EJ6.hs
@@ -0,0 +1,2 @@
+bisiesto :: Int -> Bool
+bisiesto anio = (mod anio 4 /=0) || ((mod anio 100 == 0)||(mod anio 400 /= 0))
